4c448e1b0b3b856e38fd94af1cee614d5449806b
Allenaxe <allen31042@gmail.com>
2022-05-12 21:44:47 +0800

    Update

--- project_tetris/source/volume_ctl.v
+++ project_tetris/source/volume_ctl.v
@@ -52,28 +52,25 @@ module volume_ctl(
             volume <= volume_next;   
     
     always@*
-        if (Do | Re | Mi)
-            case(volume)
-                4'd0: volume_max = 16'h0000;
-                4'd1: volume_max = 16'h0999;
-                4'd2: volume_max = 16'h0fff;
-                4'd3: volume_max = 16'h1999;
-                4'd4: volume_max = 16'h1fff;
-                4'd5: volume_max = 16'h2999;
-                4'd6: volume_max = 16'h2fff;
-                4'd7: volume_max = 16'h3999;
-                4'd8: volume_max = 16'h3fff;
-                4'd9: volume_max = 16'h4999;
-                4'd10: volume_max = 16'h4fff;
-                4'd11: volume_max = 16'h5999;
-                4'd12: volume_max = 16'h5fff;
-                4'd13: volume_max = 16'h6999;
-                4'd14: volume_max = 16'h6fff;
-                4'd15: volume_max = 16'h7999;
-                default:volume_max = 16'h0000;
-            endcase
-        else
-            volume_max = 16'h0000;
+        case(volume)
+            4'd0: volume_max = 16'h0000;
+            4'd1: volume_max = 16'h0999;
+            4'd2: volume_max = 16'h0fff;
+            4'd3: volume_max = 16'h1999;
+            4'd4: volume_max = 16'h1fff;
+            4'd5: volume_max = 16'h2999;
+            4'd6: volume_max = 16'h2fff;
+            4'd7: volume_max = 16'h3999;
+            4'd8: volume_max = 16'h3fff;
+            4'd9: volume_max = 16'h4999;
+            4'd10: volume_max = 16'h4fff;
+            4'd11: volume_max = 16'h5999;
+            4'd12: volume_max = 16'h5fff;
+            4'd13: volume_max = 16'h6999;
+            4'd14: volume_max = 16'h6fff;
+            4'd15: volume_max = 16'h7999;
+            default:volume_max = 16'h0000;
+        endcase
     
     assign volume_min = 16'h0000 - volume_max;
     
